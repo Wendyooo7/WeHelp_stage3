dd7c606c25c77c5b89c5a95db51bd51beb16ff89
Wendyooo7 <133959213+Wendyooo7@users.noreply.github.com>
2024-07-31 23:23:51 +0800

    Finished sign-up & sign-in forms in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,49 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <header>
-        <h1>Wendy の React 練習專案</h1>
-      </header>
+      <h1 className={styles.h1}>Wendy の React 練習專案</h1>
 
-      <Link className={styles.link} href="/accounting" title="點此前往">
+      <Form />
+
+      {/* <Link className={styles.link} href="/accounting" title="點此前往">
         記帳小本本
-      </Link>
+      </Link> */}
     </main>
+  );
+}
+
+function Form() {
+  return (
+    <>
+      <form className={styles.form}>
+        <h3>登入系統</h3>
+        <div className={styles.formDiv}>
+          <label htmlFor="email">電郵：</label>
+          <input type="text" className={styles.input} />
+        </div>
+
+        <div className={styles.formDiv}>
+          <label htmlFor="password">密碼：</label>
+          <input type="password" className={styles.input} />
+        </div>
+
+        <button className={styles.btn}>登入</button>
+      </form>
+
+      <form className={styles.form}>
+        <h3>註冊帳戶</h3>
+        <div className={styles.formDiv}>
+          <label htmlFor="email">電郵：</label>
+          <input type="text" className={styles.input} />
+        </div>
+
+        <div className={styles.formDiv}>
+          <label htmlFor="password">密碼：</label>
+          <input type="password" className={styles.input} />
+        </div>
+
+        <button className={styles.btn}>註冊</button>
+      </form>
+    </>
   );
 }
